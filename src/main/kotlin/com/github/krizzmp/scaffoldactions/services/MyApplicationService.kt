820b5df2640e7aeff3ba77c6037d5e93ea76b203
GitHub Action <action@github.com>
2021-08-02 10:27:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/krizzmp/scaffoldactions/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/krizzmp/scaffoldactions/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.krizzmp.scaffoldactions.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.krizzmp.scaffoldactions.MyBundle
 
 class MyApplicationService {
 
